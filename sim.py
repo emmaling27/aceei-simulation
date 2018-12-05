f31ba450419e5e991825b5d978358c3e62cb7339
AllisonKZhang <allison.kangrong.zhang@gmail.com>
2018-12-04 19:36:35 -0500

    allocate function using mip

--- sim.py
+++ sim.py
@@ -1,4 +1,5 @@
 import sys
+import cvxpy as cp
 import numpy as np
 
 def main(args):
@@ -11,8 +12,28 @@ def main(args):
 
     # for x in prices:
 
-# def allocate(utilities, caps, budgets, prices):
-
+def allocate(utilities, caps, budgets, prices):
+	# Creates course allocation variable matrix with Boolean entries
+	alloc = cp.Variable((n,2), boolean=True)
+	
+	enrollment = np.sum(alloc, axis=0)
+	
+	# Capacity constraint
+	capacity_constraint = enrollment <= caps
+	
+	payments = np.matmul(alloc, prices)
+	
+	# Budget constraint
+	budget_constraint = payments <= budgets
+			
+	
+	total_util = utilities * alloc
+	
+	problem = cp.Problem(cp.Maximize(total_util), [capacity_constraint, budget_constraint])
+	
+	solution = problem.solve()
+	
+	return np.sum(solution, axis=0)
 
 if __name__ == "__main__":
     main(sys.argv)
